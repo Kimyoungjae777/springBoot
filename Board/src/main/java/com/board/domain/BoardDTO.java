059ac1693716cec625e79436d061c87f0d454d10
kimyoungjae <kimyoungjae123@naver.com>
2022-07-15 17:21:01 +0900

    board dto

--- Board/src/main/java/com/board/domain/BoardDTO.java
+++ Board/src/main/java/com/board/domain/BoardDTO.java
@@ -1,5 +1,21 @@
 package com.board.domain;
 
+import java.time.LocalDateTime;
+
 public class BoardDTO {
+	
+	private Long idx;
+	private String title;
+	private String content;
+	private String writer;
+	private int viewcnt;
+	private String noticeYn;
+	private String secretYn;
+	private String deleteYn;
+	private LocalDateTime insertTime;
+	private LocalDateTime deleteTime;
+	private LocalDateTime updateTime;
+	
+	
 
 }
